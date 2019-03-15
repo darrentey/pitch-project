2028e0863f2f6dee11cb12ed9574336f7339a6be
Darren Tey <darrentey19@gmail.com>
2019-03-15 15:16:08 +0800

    twitter tag

--- main/blueprints/users/views.py
+++ main/blueprints/users/views.py
@@ -91,6 +91,20 @@ def update(id):
         return render_template('edit.html',errors=user.errors,user=user)
     return render_template('401.html'), 401 
 
-@users_blueprint.route("/carousel") 
+@users_blueprint.route("/carousel",methods=['GET'])
 def carousel():
-    return render_template('carousel.html')
+    insta_tag = General.get(name='instagram').contents
+    twitter_tag = General.get(name='twitter').contents
+    results = []
+    preferences = Preference.select()
+    for preference in preferences:
+        for article in preference.articles:
+            json_article = json.loads(article)
+            json_article['category']=preference.categories
+            results.append(json_article)
+    shuffle(results)
+    return render_template('carousel.html',results=results,insta_tag=insta_tag,twitter_tag=twitter_tag)
+
+@users_blueprint.route("/users_preference") 
+def users_preference():
+    return render_template('users_preference.html')

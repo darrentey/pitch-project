01a30edc44ac2099fb2039f72cb944d7a2c88eeb
Kenneth <chookw.ken@gmail.com>
2019-03-19 12:13:22 +0800

    more or less functional site

--- main/util/food.py
+++ main/util/food.py
@@ -5,61 +5,69 @@ import json
 
 food_result=[]
 
+# missing desc
 def scrape_fb1():
     soup = BeautifulSoup(urlopen('https://www.star2.com/food'), 'html.parser')
     for div in soup.find_all('div',class_='col-sm-4'):
         for img in div.find_all('img'):
             for a in div.find_all('h3'):
+                link = div.find('a')['href'],
+                # soup = BeautifulSoup(urlopen(link), 'html.parser')
+                # for meta in soup.find_all('meta', attrs={'name':'description'}):
+                #     desc = meta['content'],
                 food_result.append(json.dumps({
                     'title':a.get_text(strip=True),
-                    'link':div.find('a')['href'],
+                    'link':link,
                     'image':img.get('src'),
                 }))
-    print(food_result)
 
 def scrape_fb2():
     soup = BeautifulSoup(urlopen('https://www.thestar.com.my/metro/eat-and-drink'), 'html.parser')
     url = 'https://www.thestar.com.my'
     for div in soup.find_all('div', class_='row list-listing'):
-        for img in div.find_all('img'):
-            for p in div.find_all('p'):
+        for p in div.find_all('p'):
+            link = url+div.find('a')['href']
+            soup = BeautifulSoup(urlopen(link), 'html.parser')
+            for meta in soup.find_all('meta', attrs={'property':'og:image'}):
+                img = meta['content']
                 food_result.append(json.dumps({
-                    'title':div.find('h2').get_text(),
-                    'link':url+div.find('a')['href'],
-                    'image':url+img.get('src'),
-                    'desc':p.get_text(strip=True),
-                }))
-    print(food_result)
+                        'title':div.find('h2').get_text(),
+                        'link':link,
+                        'image':img,
+                        'desc':p.get_text(strip=True),
+                    }))
 
-# multiple printed items
+# delete [1]
 def scrape_fb3():
     soup = BeautifulSoup(urlopen('https://www.mariefranceasia.com/my/food-my'), 'html.parser')
     check=[]
     for div in soup.find_all('div', class_='col-xs-12 col-sm-6 col-md-6 item-post'):
         for img in div.find_all('img'):
             for a in div.find_all('a'):
-                link = a['href']
+                link = a['href'],
+                title = div.find('div', class_='info_title').get_text(strip=True),
                 if link not in check:
-                    food_result.append(json.dumps({
-                        'title':div.find('div', class_='info_title').get_text(strip=True),
-                        'link':link,
-                        'image':img.get('data-src'),
-                        'desc':a.get_text(strip=True),
-                    }))
-                else:
-                    return
-    print(food_result)
+                        check.append(link),
+                        food_result.append(json.dumps({
+                                    'title':title,
+                                    'link':link,
+                                    'image':img.get('data-src'),
+                                    'desc':a.get_text(strip=True),
+                                }))
 
-# repeated 2 times
 def scrape_fb4():
     soup = BeautifulSoup(urlopen('https://www.freemalaysiatoday.com/category/leisure/food'), 'html.parser')
+    check=[]
     for div in soup.find_all('div', class_='td-block-span6'):
         for img in div.find_all('img'):
             for a in div.find_all('a'):
-                food_result.append(json.dumps({
-                    'title':a['title'],
-                    'link':a['href'],
-                    'image':img.get('src'),
-                    'desc':div.find('div', class_='td-excerpt').get_text(strip=True),
-                }))
+                link = a['href']
+                if link not in check:
+                    check.append(link),
+                    food_result.append(json.dumps({
+                                    'title':a['title'],
+                                    'link':link,
+                                    'image':img.get('src'),
+                                    'desc':div.find('div', class_='td-excerpt').get_text(strip=True),
+                                }))
     print(food_result)
